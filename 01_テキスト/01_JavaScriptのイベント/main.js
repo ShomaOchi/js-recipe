91b34f7e3234b708d0dfcf5f2443c580d87d14cd
ShomaOchi <minamimachida0617@gmail.com>
2022-04-08 20:27:05 +0900

    DOM

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,22 @@
+// button 要素と click イベント
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+button.onclick = alertMessage
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
+
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
