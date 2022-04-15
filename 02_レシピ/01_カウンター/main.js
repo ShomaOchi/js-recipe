42781685c365b308d2946605cf0eb08dc032fb92
ShomaOchi <minamimachida0617@gmail.com>
2022-04-15 15:57:30 +0900

    便利なボタン

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,7 +1,12 @@
-const display = document.getElementById("display")
+const display1 = document.getElementById("display1")
 const plusButton = document.getElementById("plus-button")
-
+const display2 = document.getElementById("display2")
+const minusButton = document.getElementById("minus-button")
+const display3 = document.getElementById("display3")
+const nibaiButton = document.getElementById("nibai-button")
 let count = 0
+let count2 = 0
+let count3 = 1
 
 // ボタン要素のonclickハンドラに関数を代入する
 plusButton.onclick = function () {
@@ -9,4 +14,14 @@ plusButton.onclick = function () {
   count += 1
   // count を表示
   display.textContent = count
+}
+
+minusButton.onclick = function () {
+  count2 -= 1
+  display2.textContent = count2
+}
+
+nibaiButton.onclick = function () {
+  count3 = 2 * count3
+  display3.textContent = count3
 }
